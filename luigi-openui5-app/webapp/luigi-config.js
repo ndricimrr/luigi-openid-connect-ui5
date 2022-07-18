9b68e6ad3ce8adcbd563b71de711be317ac6d9b4
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-07-18 17:28:54 +0200

    Update luigi-config.js

--- luigi-openui5-app/webapp/luigi-config.js
+++ luigi-openui5-app/webapp/luigi-config.js
@@ -1,5 +1,5 @@
 //you can now use ES6 goodies here
-// // importing from npm package directly not possible without bundling
+// importing from npm package directly not possible without bundling. Need to bundle the luigi-config.js file through UI5 configurations to use this import
 // import oidcProvider from '@luigi-project/plugin-auth-oidc';
 
 // using workaround for UMD packages
